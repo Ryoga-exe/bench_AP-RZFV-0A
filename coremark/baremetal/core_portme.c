4017193cf8cf703d711f846ecc368f1fe5c9deb2
Ryoga-exe <git@ryoga.dev>
2025-12-02 15:31:12 +0900

    feat: fix uart and address

--- coremark/baremetal/core_portme.c
+++ coremark/baremetal/core_portme.c
@@ -351,6 +351,56 @@ ee_u8 core_stop_parallel(core_results *res) {
 
 extern int putchar(int ch);
 
+#define RZF_BOOT_ROM_BASE (0x00000000)
+#define RZF_MSRAM_BASE (0x00010000)
+#define RZF_ASRAM_BASE (0x00020000)
+#define RZF_DEVICE_BASE (0x10000000)
+#define RZF_SCIF0_BASE (0x1004B800)
+#define RZF_SPIMULT_BASE (0x10060000)
+#define RZF_SPIMULT_WBUF_BASE (0x10070000)
+#define RZF_CPG_BASE (0x11010000)
+#define RZF_SYSC_BASE (0x11020000)
+#define RZF_GPIO_BASE (0x11030000)
+#define RZF_TZC_ASRAM_BASE (0x11040000)
+#define RZF_TZC_MSRAM_BASE (0x11050000)
+#define RZF_TZC_SPI_BASE (0x11060000)
+#define RZF_TZC_DDR_BASE (0x11070000)
+#define RZF_DDR_PHY_BASE (0x11400000)
+#define RZF_DDR_MEMC_BASE (0x11410000)
+#define RZF_OTP_REG (0x11860000)
+#define RZF_SD0_BASE (0x11C00000)
+#define RZF_SD1_BASE (0x11C10000)
+#define RZF_WDT_BASE (0x12800800)
+#define RZF_SPIROM_BASE (0x20000000)
+#define RZF_DDR1_BASE (0x40000000)
+#define RZF_DDR2_BASE (0x80000000)
+#define RZF_DDR3_BASE (0x100000000)
+
+#define RZF_OTP_OTPTMPA1 (RZF_OTP_REG + 0x1178)
+#define OTPTMPA1_DEVICE_ID_FIVE_1 (0x00000000)
+#define OTPTMPA1_DEVICE_ID_FIVE_2 (0x00000001)
+#define OTPTMPA1_DEVICE_ID_MASK (0x0000000F)
+
+#define SCIF0_SMR (RZF_SCIF0_BASE + 0x0000U)
+#define SCIF0_BRR (RZF_SCIF0_BASE + 0x0002U)
+#define SCIF0_MDDR (RZF_SCIF0_BASE + 0x0002U)
+#define SCIF0_SCR (RZF_SCIF0_BASE + 0x0004U)
+#define SCIF0_FTDR (RZF_SCIF0_BASE + 0x0006U)
+#define SCIF0_FSR (RZF_SCIF0_BASE + 0x0008U)
+#define SCIF0_FRDR (RZF_SCIF0_BASE + 0x000AU)
+#define SCIF0_FCR (RZF_SCIF0_BASE + 0x000CU)
+#define SCIF0_FDR (RZF_SCIF0_BASE + 0x000EU)
+#define SCIF0_SPTR (RZF_SCIF0_BASE + 0x0010U)
+#define SCIF0_LSR (RZF_SCIF0_BASE + 0x0012U)
+#define SCIF0_SEMR (RZF_SCIF0_BASE + 0x0014U)
+
+#include <stdint.h>
+
 void uart_send_char (char ch) {
-    putchar(ch);
+  // putchar(ch);
+
+  while (!(0x60U & *((volatile uint16_t *)SCIF0_FSR)))
+    ;
+  *((volatile unsigned char *)SCIF0_FTDR) = ch;
+  *((volatile uint16_t *)SCIF0_FSR) &= ~0x60U; /* TEND,TDFE clear */
 }
